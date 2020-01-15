cb3ec8ff62c4e300be0b1c9f6b7c7a15b761395f
MancMetFeds <thomashatton0@gmail.com>
2020-01-15 23:47:52 +0000

    tutorial: created simple web server to handle request

--- swa.go
+++ swa.go
@@ -2,49 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!, r.URL.Path[1:]")
 }
-
-// pages are defined as structs
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-//expects pointer to page, returns error msg
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Print(err)
-		return nil, err // generic error handling
-	}
-
-	return &Page{ // returns pointer to a populated 'Page' struct
-		Title: title,
-		Body:  body,
-	}, nil
-}
-
 func main() {
-	p1 := &Page{
-		Title: "TestPage",
-		Body:  []byte("This is a sample Page"),
-	}
-	p1.save()
-
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
