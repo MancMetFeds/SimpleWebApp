47831d683ba73f7bc7e28b5bacab1a3e3faaf5a6
MancMetFeds <thomashatton0@gmail.com>
2020-01-16 11:36:53 +0000

    tutorial: added an editing page

--- swa.go
+++ swa.go
@@ -9,8 +9,29 @@ import (
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{
+			Title: title,
+		}
+	}
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{
+			Title: title,
+		}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>", p.Title, p.Title, p.Body)
 
 }
 
@@ -41,7 +62,9 @@ func loadPage(title string) (*Page, error) {
 	}, nil
 }
 
-func main()
+func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
