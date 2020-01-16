2d0a3e8eecbefa01031be65be4829ca7551a028a
MancMetFeds <thomashatton0@gmail.com>
2020-01-16 16:25:21 +0000

    built basic functionality of portfolio site, will further improve oprove on this as i go along.

--- portfolio.go
+++ portfolio.go
@@ -1,17 +1,69 @@
 package main
 
-func homepage() {
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadContents(t string) (*Page, error) {
+	return &Page{
+		Title: t,
+	}, nil
+}
+
+func getHTML(w http.ResponseWriter, file string, p *Page) {
+	t, err := template.ParseFiles(file + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
 }
 
-func aboutMe() {
+func viewController(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path[len("/"):]
+	p, err := loadContents(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+	if file == "" {
+		file = "index"
+		p.Title = "Home"
+	}
+	getHTML(w, file, p)
 
 }
 
-func contactDetails() {
+// func aboutMe(w http.ResponseWriter, r *http.Request) {
+// 	p, err := loadContents("about")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusNotFound)
+// 	}
+// 	getHTML(w, "about", p)
+// }
 
-}
+// func contactDetails(w http.ResponseWriter, r *http.Request) {
+// 	p, err := loadContents("contact")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusNotFound)
+// 	}
+// 	getHTML(w, "contact", p)
+// }
 
 func main() {
+	http.HandleFunc("/", viewController)
+	http.HandleFunc("/about", viewController)
+	http.HandleFunc("/contact", viewController)
 
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
